465bfe86a94c13b956db4594b88b367f8d2be3a8
John Firebaugh <john.firebaugh@gmail.com>
2016-04-29 15:37:52 -0700

    Add feature type

---
+++ include/mapbox/geometry.hpp
@@ -0,0 +1,10 @@
+#pragma once
+
+#include <mapbox/geometry/point.hpp>
+#include <mapbox/geometry/line_string.hpp>
+#include <mapbox/geometry/polygon.hpp>
+#include <mapbox/geometry/multi_point.hpp>
+#include <mapbox/geometry/multi_line_string.hpp>
+#include <mapbox/geometry/multi_polygon.hpp>
+#include <mapbox/geometry/geometry.hpp>
+#include <mapbox/geometry/feature.hpp>
---
+++ include/mapbox/geometry/feature.hpp
@@ -0,0 +1,43 @@
+#pragma once
+
+#include <mapbox/geometry/geometry.hpp>
+
+#include <mapbox/variant.hpp>
+
+#include <cstdint>
+#include <string>
+#include <vector>
+#include <unordered_map>
+
+namespace mapbox { namespace geometry {
+
+struct value;
+
+using value_base = mapbox::util::variant<bool, int64_t, uint64_t, double, std::string,
+                                         mapbox::util::recursive_wrapper<std::vector<value>>,
+                                         mapbox::util::recursive_wrapper<std::unordered_map<std::string, value>>>;
+
+struct value : value_base
+{
+    using value_base::value_base;
+};
+
+template <class T>
+struct feature
+{
+    using geometry_type = mapbox::geometry::geometry<T>; // Fully qualified to avoid GCC -fpermissive error.
+    using property_map = std::unordered_map<std::string, value>;
+
+    geometry_type geometry;
+    property_map properties {};
+};
+
+template <class T, template <typename...> class Cont = std::vector>
+struct feature_collection : Cont<feature<T>>
+{
+    using feature_type = feature<T>;
+    using container_type = Cont<feature_type>;
+    using container_type::container_type;
+};
+
+}}
