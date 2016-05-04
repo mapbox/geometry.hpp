f901b409826ffa07145105d447d6682b9ae0a390
John Firebaugh <john.firebaugh@gmail.com>
2016-05-04 10:22:35 -0700

    Don't inherit linear_ring from line_string

--- include/mapbox/geometry/polygon.hpp
+++ include/mapbox/geometry/polygon.hpp
@@ -1,18 +1,20 @@
 #pragma once
 
 // mapbox
-#include <mapbox/geometry/line_string.hpp>
+#include <mapbox/geometry/point.hpp>
 
 // stl
 #include <vector>
 
 namespace mapbox { namespace geometry {
 
-template <typename T>
-struct linear_ring : line_string<T>
+template <typename T, template <typename...> class Cont = std::vector>
+struct linear_ring : Cont<point<T>>
 {
     using coordinate_type = T;
-    using line_string<T>::line_string;
+    using point_type = point<T>;
+    using container_type = Cont<point_type>;
+    using container_type::container_type;
 };
 
 template <typename T, template <typename...> class Cont = std::vector>
