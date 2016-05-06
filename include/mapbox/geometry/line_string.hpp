ab720bb7e1eb2a44461e8dbe524a7f2c007a24ea
John Firebaugh <john.firebaugh@gmail.com>
2016-05-06 09:21:28 -0700

    Remove auxiliary methods from line_string

--- include/mapbox/geometry/line_string.hpp
+++ include/mapbox/geometry/line_string.hpp
@@ -14,9 +14,6 @@ struct line_string : Cont<point<T> >
     using point_type = point<T>;
     using container_type = Cont<point_type>;
     using container_type::container_type;
-
-    inline std::size_t num_points() const { return container_type::size(); }
-    inline void add_coord(T x, T y) { container_type::template emplace_back(x,y);}
 };
 
 }}
