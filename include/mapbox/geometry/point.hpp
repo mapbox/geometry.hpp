92560839fe67459f4b270547df895e2511d28d6d
Blake Thompson <flippmoke@gmail.com>
2017-03-27 13:41:07 -0500

    Approximately equal for floating point values.

--- include/mapbox/geometry/point.hpp
+++ include/mapbox/geometry/point.hpp
@@ -20,9 +20,27 @@ struct point
 };
 
 template <typename T>
+bool values_are_equal(T a, T b)
+{
+    return a == b;
+}
+
+template <>
+bool values_are_equal<double>(double a, double b)
+{
+    return std::fabs(a - b) < (3.0 * std::numeric_limits<double>::epsilon());
+}
+
+template <>
+bool values_are_equal<float>(float a, float b)
+{
+    return std::fabs(a - b) < (3.0 * std::numeric_limits<float>::epsilon());
+}
+
+template <typename T>
 constexpr bool operator==(point<T> const& lhs, point<T> const& rhs)
 {
-    return lhs.x == rhs.x && lhs.y == rhs.y;
+    return values_are_equal(lhs.x, rhs.x) && values_are_equal(lhs.y, rhs.y);
 }
 
 template <typename T>
